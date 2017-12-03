5d38d400941059112a312f35fbbb472a4994d328
PhilippeSawyer <philippe.sawyer@hotmail.com>
2017-12-03 18:18:03 -0500

    Update addUser.php

--- login/addUser.php
+++ login/addUser.php
@@ -4,6 +4,7 @@ $servername = "localhost";
 $username = "comp307";
 $password = "password";
 $database = "USER_BASE";
+$database2 = "USER_EVENTS";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
@@ -12,14 +13,37 @@ $newPassword = $_POST['password'];
 $newEmail = $_POST['email'];
 
 
-$sql = "INSERT INTO USERS (username, password, email)
-VALUES ('$newName', '$newPassword', '$newEmail')";
+	// Check if the username is available:
+	$sql = "SELECT id FROM USERS WHERE username = '$newName'";
+	$result = mysqli_query($conn,$sql);
+	$count = mysqli_num_rows($result);
 
-if ($conn->query($sql) === TRUE) {
-	echo "New account created successfully !";
-} else {
-	echo "Error, try again later ";
-}
+	
+	if($count == 1){
+		
+	exit("Unavailable username.");
 
+	}else if(empty($newName) or empty($newPassword) or empty($newEmail) ){
+		
+		exit("Missing information.");
+		
+	}else{	// Create the account:
+	
+	
+		$sql = "INSERT INTO USERS (username, password, email)
+		VALUES ('$newName', '$newPassword', '$newEmail')";
+		
+		// add a table to store the events
+		$conn2 = new mysqli($servername, $username, $password, $database2);
+		$sql2 = "CREATE TABLE `{$newName}` (
+		id INT(11))";
+		
+		
+		if ($conn->query($sql) === TRUE && $conn2->query($sql2) === TRUE) {
+			echo "New account created successfully !";
+		} else {
+			echo "Error, try again later ";
+		}
+	}
 $conn->close();
 ?>
