ae3e708aff13162394f55fbc9124ff732951e118
mslav <mslavslav@gmail.com>
2017-12-03 15:39:51 -0500

    Add files via upload

---
+++ MakeEvent/makeEvent.php
@@ -0,0 +1,24 @@
+<?php 
+
+$servername = "localhost";
+$username = "comp307";
+$password = "password";
+$database = "USER_BASE";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+$newName = $_POST['name'];
+$newDate = $_POST['date'];
+
+
+$sql = "INSERT INTO EVENTS (name, date)
+VALUES ('$newName', '$newDate')";
+
+if ($conn->query($sql) === TRUE) {
+	echo "Event Created!";
+} else {
+	echo "Error, try again later ";
+}
+
+$conn->close();
+?>
